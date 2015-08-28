5358fbcc45554364e8ef73d683a4099f982b9ab0
tonydeluce111 <tondeluce111@gmail.com>
2015-08-27 21:34:48 -0700

    this class simply has one method that returns the total percentage of Positive messages currently in the miniTwitter application.

---
+++ TotalPercentagePositiveMessages.java
@@ -0,0 +1,9 @@
+package us.deluce.minitwitter;
+
+public class TotalPercentagePositiveMessages {
+	
+	public static float totalNumberOfPositiveMessages() {		
+		return User.getTotalPositivePercentageOfMessages();
+	}
+
+}
